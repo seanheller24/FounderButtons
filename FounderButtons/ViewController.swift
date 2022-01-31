a5ad89a2885586a7f70523de19547c9db119ca25
seanheller24 <hellerse@bc.edu>
2022-01-31 17:17:20 -0500

    Update 1

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func googleButtonPressed(_ sender: UIButton) {
+        topLabel.text = "Larry Page"
+        bottomLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func rtrButtonPressed(_ sender: UIButton) {
+        topLabel.text = "Jennifer Hyman"
+        bottomLabel.text = "Jenny Fleiss"
+    }
+    
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        topLabel.text = ""
+        bottomLabel.text = ""
+    }
+    
 }
 
